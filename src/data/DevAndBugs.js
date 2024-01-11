4e4a8db3dfb464dc78f2ab7497bd0e070b9afb57
Petri  Räsänen <petri.rasanen@schibsted.com>
2024-01-11 16:52:23 +0200

    dab_updts

--- src/data/DevAndBugs.js
+++ src/data/DevAndBugs.js
@@ -3,6 +3,8 @@
  * DEV:
  *  add concealed weapons
  *  add rounding to speed (note, that one mod rounds to up)
+ *  button to go back to first page
+ *  make loading and looking saved rigs possible
  * 
  * BUGS:
  *  it is possible to trick the cost when adding and deleting mines in "incorrect" order
