a2e11c059936e9c93e444464cc8e948bb5447dee
Petri  Räsänen <petri.rasanen@schibsted.com>
2024-10-13 00:58:19 +0300

    defending home wip

---
+++ src/data/books/defendingHome.ts
@@ -0,0 +1,56 @@
+import { BookType } from "../../components/bookComponents/Book";
+
+export const template: BookType = {
+
+    title: 'Defending Home',
+    paragraphs: [
+        {
+            subTitle: `Back to home`,
+            locationStr: `Nii'vaat, air, near N1 base`,
+            strings: `
+                I was flying our cutter over the clouds. Due the weather conditions, that's usually the best way. It was already dark, Annika and Arle were sleeping in their cabins and dogs, probably with them. I was very tired too, but luckily we were near home. We were coming back from hard Charred Axle tour. We took on seasoned Team Warhead, in four fight series.
+                The first match was in Thynakia. I drove Starblade and Arle was a gunner, equipped with reduction lance and two sets of mines. I started the match with a horrible driving error and crashed our rig to rock, resulting us to being a sitting duck target for very capable all-terrain roller rig named Star Flame. And that was not the worst. I repeated same kind of driving error later on match too.
+                However, while we took some serious damage, Axel gunning like a master gunner he is, our both rigs were about one shot away from being charred, something crazy happened. Falgor appeared and took the Star Flame away and trashed it. So we got the W, but I was not happy about those errors.
+                `
+        },
+        {
+            subTitle: ``,
+            strings: `
+                After that, we came back home to Nii'vaat and fought another hard match with our Leprechaun versus a slammer rig named Daisy. We were able to keep his slam damage quite slow, and our heavy variant rattler made his fire inaccurate, eating chunks of his formidable armour, bit by bit, until it was charred. Also got to mention, that our Comet fire drone's second eye module made our fire very accurate.
+                `
+        },
+        {
+            subTitle: ``,
+            strings: `
+                Right after that, we packed our cutter again with rigs Ice and Avispa headead out to Ainash, were this time I went to speedy Ice versus heavy hitting miner rig named Hammer of Ruin. I think I could have had a good change to battle for victory too, however as Falgor was our friend in Thynakia, here local phenomenom brought the Terror Worm near my rig Ice and my evasing maneuvers ate my speed and the worm ate my armour plates... The rest was just butchery, I got charred.
+                `
+        },
+        {
+            subTitle: ``,
+            strings: `
+                To finish the tour we went to Formax Theta, leading 2-1. We had our swamp stomper, the Avispa, with very heavy weapons outfit, and we planned to interrupt our opponents, the Sideshows shooting with our accelerator and then come back with lots of swarm missiles. The battle went well, but Sideshow was bit more effective, than us avoiding hits and took earned victory and evened the series to 2-2. However, it was still a very good result for us, against so good team.
+                `
+        },
+        {
+            subTitle: ``,
+            strings: `
+                I got beep to the console, that came automated message from anti-air systems of our base. It reads as: "You are approaching private air space. Send your identification as reply, or turn your course, or we use deadly force". I replied our ID, as I didn't want to try to dodge our own anti-air missile swarms. I got a reply "Welcome back, team!". Few minutes after that, I took us below the clouds and saw our base. I sent a message to our base, to open our airfield dome. We had build a domed, mostly underground hangar to a big explosion crater. The dome started to open, while I steered our cutter towards it. I activated the landing gear and landing thrusters, the cutter shuttered a bit, that seemed to wake up my wife and son.
+                "We home?", Annika asked. "Yeah, or atleast soon", I replied. *THUDD*, we landed on hangar. I set some commands to the console, and the dome started to close and our main engines shutted down. Finally at home! We decided to leave charred rigs to the cutter for now, and just took our personal stuff, and walked throught the tunnel to the apartment building, after letting dogs out.
+            `
+        },
+        {
+            subTitle: `What next and how?`,
+            locationStr: `Nii'vaat, Amorcitos Ranch, N1 base`,
+            strings: `
+                After long sleep, we all found ourself at the apartment block eating a brunch. We talked a bit about the situation and we all agreed, that we need more crew, or start to participate in less events. Also, leaving the base without crew, while we were in events was very risky. We had some automatic defense systems, that could fend off some random raiders, but in Nii'vaat there are factions, that could quite easily destroy them. By this time we had some friends already at Junctions Town, so we had some names on our mind, but would they want to work with us and under what conditions? Those are good questions, but we decided to open negotiations.
+                `
+        },
+        {
+            subTitle: `Night Rider?`,
+            locationStr: `The Pit, The trough, Junctions Town`,
+            strings: `
+                I was at corner table of The Pit. Slowly drinking beer. This was third night in row when I was here, because I wanted to meet someone who should regulary wisit the place. I finished my pint and was ready to go home, when the front door opened and small cat walked in, took some running steps and jumped to a bar counter. "Hi Lydia!", said the barkeeper Nena, tough human female, who runned the place, and placed a small plate of raw meat to front of the cat, who eagerly started to eat it. Shortly after the cat a big man walked in. It has been many years I had last seen him. 
+                `
+        },
+    ]
+};
